4e964d95c6ea16d6bc26810e63ed2aeaf3ebfe91
Mateo <mateo_maturano@hotmail.com>
2021-07-01 17:02:41 -0300

    pantalla notificacion match

--- back/app/Http/Controllers/api/OrdersController.php
+++ back/app/Http/Controllers/api/OrdersController.php
@@ -28,7 +28,7 @@ class OrdersController extends Controller
     public function OrdersByUser($id)
     {
         $ordersByUser = DB::table('orders')
-            ->select('orders.id_order', 'houser.orders.fk_houser', 'houser.user.name', 'houser.user.avatar', 'services.title', 'orders_states.state', 'orders.created_at', 'orders.user_message', 'orders.houser_message', 'orders.read_at')
+            ->select('orders.id_order', 'houser.orders.fk_houser', 'houser.user.name', 'houser.user.avatar', 'houser.user.portrait', 'houser.user.alt', 'services.title', 'orders_states.state', 'orders.created_at', 'orders.user_message', 'orders.houser_message', 'orders.read_at')
             ->join('houser.user', 'orders.fk_houser', '=', 'houser.user.id_user')
             ->join('houser.services', 'orders.fk_service', '=', 'services.id_service')
             ->join('houser.orders_states', 'orders.fk_order_state', '=', 'orders_states.id_order_state')
